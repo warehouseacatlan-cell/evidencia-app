c5b34e8c5f8e6d7ba821b75f1110c0e29a57b104
warehouseacatlan-cell <warehouseacatlan@gmail.com>
2026-03-20 17:32:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
+  res.send("YA FUNCIONA 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor listo"));

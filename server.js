45c676df4956d49b94fcba000bd3ceab6fb9926a
warehouseacatlan-cell <warehouseacatlan@gmail.com>
2026-03-20 18:05:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// Ruta de prueba
 app.get("/", (req, res) => {
-  res.send("YA FUNCIONA 🚀");
+  res.send("API funcionando 🚀");
+});
+
+// Ejemplo de endpoint
+app.get("/api/test", (req, res) => {
+  res.json({ mensaje: "Todo bien" });
 });
 
 const PORT = process.env.PORT || 3000;

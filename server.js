c46377abf8a01b7ceb4a9c633a94e09ec775bdf1
warehouseacatlan-cell <warehouseacatlan@gmail.com>
2026-03-21 09:53:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const multer = require("multer");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Guardado en memoria (temporal)
